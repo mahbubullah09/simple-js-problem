698788599689c723316dfc79518ca3f8e6fbcf22
Mahbubullah <mahbubullahpathan@gmail.com>
2023-08-27 19:39:09 +0600

    biyeshadhi problem

---
+++ biyeshadhi.js
@@ -0,0 +1,15 @@
+//select a patro from this object .If patro has a govt job then salary must be greater than 20000 & if patro has non-govt job then salary must be greater than 40000 
+
+
+
+
+const patro = [
+    { name: "Abdullah", job: "govt", salary: 17000 },
+    { name: "Abbas", job: "non-govt", salary: 25000 },
+    { name: "Karim", job: "govt", salary: 21000 },
+    { name: "Hasib", job: "non-govt", salary: 47000 },
+    { name: "Jamil", job: "govt", salary: 23000 },
+    { name: "Mehedi", job: "non-govt", salary: 55000 },
+  ];
+
+
